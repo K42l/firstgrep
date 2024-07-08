9719bcc0f46f46ef3f2564d9c2427147c3b491d4
K42l <82630877+K42l@users.noreply.github.com>
2024-07-08 00:33:47 -0300

    main update
    Fixed the progress bar and added the option to write to a file

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,39 @@ use std::io::{BufReader, BufWriter};
 use clap::Parser;
 use std::io::{self, Write};
 use indicatif::ProgressBar;
-use std::thread;
-use std::time::Duration;
-
+//use std::time::Duration;
 
 #[derive(Parser)]
 struct Cli {
     string_pattern: String,
-    file_path: std::path::PathBuf
+    file_path: std::path::PathBuf,
+    output_path: Option<String>
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let stdout = io::stdout();
-    let mut handle = BufWriter::new(stdout.lock());
     let args = Cli::parse();
+    let stdout = io::stdout();
+    //let path = args.output_path;
+    let mut handle = BufWriter::new(match &args.output_path{
+        Some(ref path) => Box::new(File::create(&path).unwrap()) as Box<dyn Write>,
+        None => Box::new(stdout) as Box<dyn Write>
+    });
+    //let mut handle = BufWriter::new(stdout.lock());s
     let mut lines_found  = 0;
     //let mut count = 0;
     match File::open(&args.file_path){
-        Ok(file_content) =>{
-            let pb = ProgressBar::new(5159);
-            let reader = BufReader::new(file_content);
-            for line in reader.lines(){
+        Ok(mut file_content) =>{
+            let lines_amount = BufReader::new(&file_content).lines().count();
+            let pb = ProgressBar::new(lines_amount.try_into().unwrap());
+            let _ = file_content.seek(std::io::SeekFrom::Start(0));
+            let reader = BufReader::new(&file_content);
+            for (i, line) in reader.lines().enumerate(){
                 match line{
                     Ok(content) =>{
                         if content.contains(&args.string_pattern){
-                            //println!("line : {}", content);
-                            writeln!(handle, "line : {}", content)?;
+                            writeln!(handle, "line {i} : {:?}", content)?;
                             lines_found += 1;
                         };
-                        thread::sleep(Duration::from_millis(5));
                         pb.inc(1);
                     },
                     Err(error) =>{
